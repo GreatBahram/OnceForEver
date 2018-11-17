ad77b6edb56f3445a07c3398361b92e642e7178e
GreatBahram <aghaee.bahram@gmail.com>
2018-11-17 08:39:32 +0330

    add single threaded cpubound job into multiprocessing directory

---
+++ code_snippets/parallelism/part2-multiprocessing/single_threaded_cpubound.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import time
+
+COUNT = 85_000_000
+
+def countdown(number):
+    while number > 0:
+        number -= 1
+
+if __name__ == '__main__':
+    print('Start of program.')
+
+    start_time = time.time()
+
+    countdown(COUNT)
+
+    end_time = time.time()
+
+    print('End of program.')
+
+    print(f'Countdown took {int(end_time - start_time)} seconds')
